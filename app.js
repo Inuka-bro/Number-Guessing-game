29c2f630a3b1555caa4c1d83d17b179a2f34b8ad
Inuka-bro <inukaasith30@gmail.com>
2025-06-07 23:56:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,65 @@
+let RandomNum = Math.floor(Math.random() * 10) + 1;
+let i = 3;
+console.log(RandomNum);
+let boolean = false;
+let lblH1 = document.getElementById("h1Lbl");
+function clickAction() {
+
+    if (boolean != true) {
+        
+        let inputText = document.getElementById("txtNum");
+
+        GuessNum = inputText.value;
+
+        if (RandomNum < GuessNum) {
+            document.getElementById("img").innerHTML = '<img src="img/try-again-never-again.gif" class="card-img-top" alt="" srcset="">';
+            lblH1.innerHTML = `Your Guess Number is higher than Random Number!!You have ${i} chances`;
+
+
+        }
+        else if (RandomNum > GuessNum) {
+            document.getElementById("img").innerHTML = '<img src="img/try-again-never-again.gif" class="card-img-top" alt="" srcset="">';
+            lblH1.innerHTML = `Your Guess Number is Lower than Random Number!!You have ${i} chances`;
+
+        }
+        else if (RandomNum == GuessNum) {
+            document.getElementById("img").innerHTML = '<img src="img/200.webp" class="card-img-top" alt="" srcset="">';
+            lblH1.innerHTML = "Your Guess Number is correct";
+            boolean=true;
+            return;
+
+
+        }
+        if (i <= 0) {
+            if (RandomNum < GuessNum) {
+                document.getElementById("img").innerHTML = '<img src="img/gameover.gif" class="card-img-top" alt="" srcset="">';
+                lblH1.innerHTML = "Your game over";
+                return;
+
+            }
+            else if (RandomNum > GuessNum) {
+                document.getElementById("img").innerHTML = '<img src="img/gameover.gif" class="card-img-top" alt="" srcset="">';
+                lblH1.innerHTML = "Your game over";
+                return;
+            }
+            else if (RandomNum == GuessNum) {
+                document.getElementById("img").innerHTML = '<img src="img/200.webp" class="card-img-top" alt="" srcset="">';
+                lblH1.innerHTML = "Your Guess Number is correct";
+                boolean=true;
+                return;
+
+
+            }
+
+
+        }
+
+
+        i--;
+    }
+    else if(boolean == true){
+        lblH1.innerHTML = "You won.If you want to restart the game,Restart this website";
+    }
+
+}
+
