4c6f71152d4ebd27c6ce81fb43c33992c7bb3ad0
Inuka Asith <inukaasith30@gmail.com>
2025-06-08 23:07:44 +0530

    Update ele

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 let RandomNum = Math.floor(Math.random() * 10) + 1;
 let i = 3;
 console.log(RandomNum);
-let boolean = false;
 let lblH1 = document.getElementById("h1Lbl");
+let inputText = document.getElementById("txtNum");
+let GuessNum;
+
 function clickAction() {
-
-    if (boolean != true) {
-        
-        let inputText = document.getElementById("txtNum");
-
-        GuessNum = inputText.value;
-
+    GuessNum=inputText.value;
+    if (i>0) {
         if (RandomNum < GuessNum) {
             document.getElementById("img").innerHTML = '<img src="img/try-again-never-again.gif" class="card-img-top" alt="" srcset="">';
             lblH1.innerHTML = `Your Guess Number is higher than Random Number!!You have ${i} chances`;
@@ -30,7 +27,8 @@ function clickAction() {
 
 
         }
-        if (i == 0) {
+    }
+    if (i == 0) {
             if (RandomNum < GuessNum) {
                 document.getElementById("img").innerHTML = '<img src="img/gameover.gif" class="card-img-top" alt="" srcset="">';
                 lblH1.innerHTML = "Game over,You have no chances";
@@ -45,36 +43,17 @@ function clickAction() {
             else if (RandomNum == GuessNum) {
                 document.getElementById("img").innerHTML = '<img src="img/200.webp" class="card-img-top" alt="" srcset="">';
                 lblH1.innerHTML = "Your Guess Number is correct";
-                boolean=true;
+                
                 return;
             }
 
-        }
-        if (i <= -1) {
-            if (RandomNum < GuessNum) {
-                document.getElementById("img").innerHTML = '<img src="img/gameover.gif" class="card-img-top" alt="" srcset="">';
-                lblH1.innerHTML = "Game over,You have no chances";
-                
-
-            }
-            else if (RandomNum > GuessNum) {
-                document.getElementById("img").innerHTML = '<img src="img/gameover.gif" class="card-img-top" alt="" srcset="">';
-                lblH1.innerHTML = "Game over,You have no chances";
-                
-            }
-            else if (RandomNum == GuessNum) {
-                document.getElementById("img").innerHTML = '<img src="img/gameover.gif" class="card-img-top" alt="" srcset="">';
-                lblH1.innerHTML = "Game over,You have no chances";
-                
-            }
+    }
+    if (i <= 0) {
+            document.getElementById("img").innerHTML = '<img src="img/gameover.gif" class="card-img-top" alt="" srcset="">';
+            lblH1.innerHTML = "Game over,You have no chances";
             return;
-
-        }
-        i--;
-    }
-    else if(boolean == true){
-        lblH1.innerHTML = "You won.If you want to restart the game,Restart this website";
-    }
+    }   
+    i--;
 
 }
 
