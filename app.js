157c437d9c9a60542b553c42e9f91e73b847b5ad
Inuka Asith <inukaasith30@gmail.com>
2025-06-08 23:09:27 +0530

    Update elements

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ function clickAction() {
             }
 
     }
-    if (i <= 0) {
+    if (i <= -1) {
             document.getElementById("img").innerHTML = '<img src="img/gameover.gif" class="card-img-top" alt="" srcset="">';
             lblH1.innerHTML = "Game over,You have no chances";
             return;
